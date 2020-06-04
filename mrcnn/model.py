c33aef5efa7d2fed77c6f171670ebda7bc881bbc
DamSaulGoodMan <capron.damien@laposte.net>
2020-06-04 23:05:09 +0200

    resolve: AttributeError: 'Model' object has no attribute 'metrics_tensors'

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2199,6 +2199,7 @@ class MaskRCNN():
             loss = (
                     tf.reduce_mean(layer.output, keepdims=True)
                     * self.config.LOSS_WEIGHTS.get(name, 1.))
+            self.keras_model.metrics_tensors = []
             self.keras_model.metrics_tensors.append(loss)
 
     def set_trainable(self, layer_regex, keras_model=None, indent=0, verbose=1):
